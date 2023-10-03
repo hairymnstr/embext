0ecb82fe6960a1f373e08ec425458e7978c6cb80
Nathan Dumont <nathan@nathandumont.com>
2023-10-03 21:17:52 +0100

    Modernised the existing python test

--- test/ext_tests.py
+++ test/ext_tests.py
@@ -1,65 +1,54 @@
-#!/usr/bin/env python
-import sys, os, subprocess, hashlib
+#!/usr/bin/env python3
+import sys
+import os
+from subprocess import check_call, PIPE
+import hashlib
 from PIL import Image, ImageDraw
 
 force_create = False
 
 if (not os.path.exists("testext.img")) or (force_create):
     
-    # make the empty file for the filesystem
-    pr = subprocess.Popen(["dd", "if=/dev/zero", "of=testext.img", "bs=1M", "count=512"])
-    pr.wait()
-
-    # format the file system
-    pr = subprocess.Popen(["mke2fs", "testext.img"], stdin=subprocess.PIPE)
-    pr.stdin.write("y\n")
-    pr.wait()
+    # make an empty filesystem for test
+    check_call(["mke2fs", "testext.img", "512M"])
 
 if not os.path.isdir("temp"):
     os.mkdir("temp")
 
-print "Mount image..."    
-pr = subprocess.Popen(["sudo", "mount", "-o", "loop", "testext.img", "temp"])
-pr.wait()
+print("Mount image...")
+
+check_call(["sudo", "mount", "-o", "loop", "testext.img", "temp"])
 
 if not os.path.isdir("temp/static"):
-    print "Creating directory on new volume"
-    pr = subprocess.Popen(["sudo", "mkdir", "temp/static"])
-    pr.wait()
-    print "Making the directory writeable"
-    pr = subprocess.Popen(["sudo", "chmod", "0777", "temp/static"])
-    pr.wait()
+    print("Creating directory on new volume")
+    check_call(["sudo", "mkdir", "temp/static"])
+    print("Making the directory writeable")
+    check_call(["sudo", "chmod", "a+rwx", "temp/static"])
 
 if not os.path.exists("temp/static/test_image.png"):
-    print "Creating a test image"
+    print("Creating a test image")
     image = Image.new('RGBA', (640, 480))
     draw = ImageDraw.Draw(image)
     draw.ellipse((120, 40, 520, 440), fill = 'blue', outline = 'blue')
-    print "Saving to test file system"
+    print("Saving to test file system")
     image.save("temp/static/test_image.png")
-    fr = open("temp/static/test_image.png", "rb")
-    m = hashlib.md5()
-    m.update(fr.read())
-    fr.close()
-    fw = open("test_image.md5", "wb")
-    fw.write(m.digest())
-    fw.close()
-
-if not os.path.exists("temp/logs"):
-    print "Creating log folder"
-    pr = subprocess.Popen(["sudo", "mkdir", "temp/logs"])
-    pr.wait()
+    with open("temp/static/test_image.png", "rb") as fr:
+        m = hashlib.md5()
+        m.update(fr.read())
+    with open("test_image.md5", "wb") as fw:
+        fw.write(m.digest())
     
-    print "Making the directory writeable"
-    pr = subprocess.Popen(["sudo", "chmod", "0777", "temp/logs"])
-    pr.wait()
+if not os.path.exists("temp/logs"):
+    print("Creating log folder")
+    check_call(["sudo", "mkdir", "temp/logs"])
+
+    print("Making the directory writeable")
+    check_call(["sudo", "chmod", "0777", "temp/logs"])
 
 if not os.path.exists("temp/logs/test.txt"):
-    print "Creating test text file"
-    fw = open("temp/logs/test.txt", "w")
-    fw.write("Hello world\n")
-    fw.close()
+    print("Creating test text file")
+    with open("temp/logs/test.txt", "w") as fw:
+        fw.write("Hello world\n")
     
-print "Unmount image..."
-pr = subprocess.Popen(["sudo", "umount", "temp"])
-pr.wait()
+print("Unmount image...")
+check_call(["sudo", "umount", "temp"])
